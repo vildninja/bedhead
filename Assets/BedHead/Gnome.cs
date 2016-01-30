f1c1e18be8a2b8a0f3c7c01adc4ad38cfa6ffb68
Jannek <jannek.ek@gmail.com>
2016-01-30 15:55:07 +0100

    Init

---
+++ Assets/BedHead/Gnome.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gnome : MonoBehaviour
+{
+
+    private Animator animator;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    animator = GetComponentInChildren<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+
+    public void Walk()
+    {
+        
+    }
+}
